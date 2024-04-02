4f79179db894371a551ee23d6023a995686368ed
RobbeVDE <robbe.vandereeckt@gmail.com>
2024-04-02 16:05:50 +0200

    Update HP tuning with transformations

--- Optuna.py
+++ Optuna.py
@@ -6,7 +6,7 @@ from Data.Featurisation import Featurisation
 from optuna.trial import TrialState
 import logging
 import sys
-
+installation_id = "3437BD60"
 def objective(trial):
 
     # Generate the optimizersa and hyperparameters
@@ -26,69 +26,81 @@ def objective(trial):
     batch_size_target = trial.suggest_int("Batch_size_target", 1,64)
     # Get data
 
-    source_range = pd.date_range("2016-05-01","2019-12-31 23:00", freq='h', tz="UTC")
+    source_range = pd.date_range("2016-05-01","2020-07-31 23:00", freq='h', tz="UTC")
     target_range = pd.date_range("2020-08-01", "2020-08-31 23:00", freq='h', tz="UTC")
     eval_range = pd.date_range("2020-09-01", "2021-07-31 23:00", tz="UTC", freq='h')
 
     pvgis = pd.read_pickle('Data/PVGIS.pickle')
 
     CEDA = pd.read_pickle("CEDA_dataNL.pickle")
+    is_day = pd.read_pickle("Data/is_day.pickle")
 
     power = pd.read_pickle("Data/NL_power.pickle")
-
+    metadata = pd.read_csv("Data/installations Netherlands.csv", sep=';')
+    metadata = metadata.set_index('id')
+    metadata_id = metadata.loc[installation_id]
+    tilt = metadata_id["Tilt"]
+    peakPower = metadata_id["Watt Peak"]
+    azimuth = metadata_id["Orientation"]
+    latitude = metadata_id["Latitude"]
+    longitude = metadata_id["Longitude"]
     
-
+    ## Source data
     source_pvgis = data_slicer(pvgis, source_range)
     source_CEDA = data_slicer(CEDA, source_range)
 
 
     data = pd.merge(source_pvgis, source_CEDA, left_index=True, right_index=True)
+    data = pd.merge(data, is_day, right_index=True, left_index=True)
 
     data = [data] # Put it in a list to work with featurisation object
-
-    #2. Featurise data & ready for training & testing 7
     data = Featurisation(data)
     data.data = data.cyclic_features()
     data.data = data.add_shift('P')
     data.data = data.cyclic_angle('wind_direction_10m')
+    data.data = data.PoA(latitude, longitude, tilt, azimuth)
+    data.data = data.remove_outliers(tolerance=50)
 
 
     source_dataset = data.data[0]
    
-    min = source_dataset.min(axis=0)
-    max = source_dataset.max(axis=0)
+    min = source_dataset.min(axis=0).to_dict()
+    max = source_dataset.max(axis=0).to_dict()
 
-    #target and evaluation data will stay the same most probablyy
     target_CEDA = data_slicer(CEDA, target_range)
     target_power = data_slicer(power, target_range)
     eval_CEDA = data_slicer(CEDA, eval_range)
     eval_power = data_slicer(power, eval_range)
 
     #TARGET DATASET
-    target_dataset = pd.merge(target_power, target_CEDA, left_index=True, right_index=True, how='inner')
-    data = [target_dataset] # Put it in a list to work with featurisation object
-
-    #2. Featurise data & ready for training & testing 7
+    data = pd.merge(target_power, target_CEDA, left_index=True, right_index=True, how='inner')
+    data = pd.merge(data, is_day, right_index=True, left_index=True)
+    data = [data] # Put it in a list to work with featurisation object
     data = Featurisation(data)
     data.data = data.cyclic_features()
     data.data = data.add_shift('P')
     data.data = data.cyclic_angle('wind_direction_10m')
+    data.data = data.PoA(latitude, longitude, tilt, azimuth)
+    data.data = data.remove_outliers(tolerance=50)
 
     target_dataset = data.data[0]
 
     ## EVALUATION DATASET
-    eval_dataset = pd.merge(eval_power, eval_CEDA, left_index=True, right_index=True, how='inner')
-    data = [eval_dataset] # Put it in a list to work with featurisation object
-
-    #2. Featurise data & ready for training & testing 7
+    data = pd.merge(eval_power, eval_CEDA, left_index=True, right_index=True, how='inner')
+    data = pd.merge(data, is_day, right_index=True, left_index=True)
+    data = [data] # Put it in a list to work with featurisation object
     data = Featurisation(data)
     data.data = data.cyclic_features()
     data.data = data.add_shift('P')
     data.data = data.cyclic_angle('wind_direction_10m')
+    data.data = data.PoA(latitude, longitude, tilt, azimuth)
+    data.data = data.remove_outliers(tolerance=50)
 
     eval_dataset = data.data[0]
 
     features = list(source_dataset.columns)
+    features = ['P', 'temperature_1_5m', 'relative_humidity_1_5m', 'diffuse_surface_SW_flux', 'direct_surface_SW_flux', 'downward_surface_SW_flux', 'PoA', 'P_24h_shift', "is_day"]
+
     
     accuracy = forecast_maker(source_dataset, target_dataset, features, eval_dataset, trial, optimizer_name, lr_source, lr_target,
                                n_layers_source, n_layers_target, n_nodes_source, n_nodes_target, batch_size_source, batch_size_target,dropout, [min,max])
@@ -99,8 +111,8 @@ def objective(trial):
 if __name__ == "__main__":
 
     optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-    study_name = "example-study"  # Unique identifier of the study.
-    storage_name = "sqlite:///{}.db".format(study_name)
+    study_name = "No_feature_select"  # Unique identifier of the study.
+    storage_name = "sqlite:///HP_source.db"
     study = optuna.create_study(study_name=study_name, storage=storage_name, direction="minimize", load_if_exists=True)
     study.optimize(objective, n_trials=100)
 
