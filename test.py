52f767a4443197e3d62bfc63f0e8550913fc59b0
RobbeVDE <robbe.vandereeckt@gmail.com>
2024-04-10 09:43:11 +0200

    Saved ideal HP step 1

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from hyperparameters.hyperparameters import hyperparameters_source
-batch_size = 4
-lr = 1.75e-3
-dropout= 0.11
-n_layers = 2
-n_nodes = 190
-optimizer_name = "RMSprop"
+batch_size = 40
+lr = 1e-4
+dropout= 0.31
+n_layers = 3
+n_nodes = 454
+optimizer_name = "Adam"
 hp = hyperparameters_source(optimizer_name, lr, n_layers, n_nodes, dropout, batch_size)
 hp.save(1)
 
