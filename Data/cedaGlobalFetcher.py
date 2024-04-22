eae98b0e32a534def12ec4a6f1a02da28bd7cace
r0778797 <r0778797@esat.kuleuven.be>
2024-04-22 21:16:12 +0200

    CEDA data for Costa Rica

--- Data/cedaGlobalFetcher.py
+++ Data/cedaGlobalFetcher.py
@@ -2,6 +2,7 @@ from ftplib import FTP
 import os
 import xarray as xr
 import time
+import numpy as np
 import pandas as pd
 # Important libraries
 import os
@@ -17,7 +18,7 @@ total_df = pd.DataFrame()
 
 
 # Define the local directory name to put data in
-ddir="C:/Users/Robbe/PycharmProjects/SolNet 2/CostaRica"
+ddir="/users/students/r0778797/SolNet-2/CostaRica"
 
 # If directory doesn't exist make it
 if not os.path.isdir(ddir):
@@ -28,9 +29,9 @@ os.chdir(ddir)
 
 f=FTP("ftp.ceda.ac.uk", os.getenv('ceda_usrname'), os.getenv('ceda_pssword'))
 # loop through years
-for year in range(2016,2021):
-    if year == 2016: #Only some months present
-        month_range = range(5,13)
+for year in range(2016,2023):
+    if year == 2016: #Only some months presen
+        month_range = range(10,13)
     else:
         month_range = range(1,13)
     # loop through months
@@ -41,8 +42,8 @@ for year in range(2016,2021):
             ndays=29
         else:
             ndays=int("dummy 31 28 31 30 31 30 31 31 30 31 30 31".split()[month])
-        if (year==2016) and (month==3):
-            start_day = 1
+        if (year==2016) and (month==10):
+            start_day = 25
         # loop through days
         for day in range(start_day,ndays+1):
             day_df = pd.DataFrame()
@@ -53,37 +54,44 @@ for year in range(2016,2021):
                     day = "0" + str(day)
                 if month < 10:
                     month = "0" + str(month)
-                f.cwd(f"/badc/ukmo-nwp/data/global-grib/{year}/{month}/{day}")
-                # define filename
-                file=f"{year}{month}{day}00_WSGlobal17km_{var}_Area{area}_000144.grib"
-                # get the remote file to the local directory
-                start_timing = time.time()
-                f.retrbinary("RETR %s" % file, open(file, "wb").write)
-                df = xr.load_dataset(file, engine='cfgrib')
-                df.load()
-                df = df.sel(latitude=latitude,longitude= longitude, method="nearest").to_dataframe()
-                os.remove(file)
-                os.remove(f'{file}.923a8.idx') # Some sort of database file that Windows automatically generates
-
+                try:
+                    f.cwd(f"/badc/ukmo-nwp/data/global-grib/{year}/{month}/{day}")
+                    # define filename
+                    file=f"{year}{month}{day}00_WSGlobal17km_{var}_Area{area}_000144.grib"
+                    # get the remote file to the local directory
+                    start_timing = time.time()
+                    f.retrbinary("RETR %s" % file, open(file, "wb").write)
+                    df = xr.load_dataset(file, engine='cfgrib')
+                    df.load()
+                    df = df.sel(latitude=latitude,longitude= longitude, method="nearest").to_dataframe()
+                    os.remove(file)
+                    try:
+                        os.remove(f'{file}.9093e.idx') # Some sort of database file that Windows automatically generates
+                    except:
+                        pass
                 #make dataframe
-                df.set_index("valid_time", inplace=True)
-                end_day = int(day)+1
-                pred_time = f"{year}-{month}-{day}"
-                df = df[pred_time:pred_time]
-                print(df)
-                df = df.iloc[:,-1] #last column is actual variable, others are mjeh
-                df.name = var
-                day_df = day_df.join(df, how="right")
-                print(day_df)
-                print(f'-----{time.time()-start_timing} seconds----')
+                    df.set_index("valid_time", inplace=True)
+                    end_day = int(day)+1
+                    pred_time = f"{year}-{month}-{day}"
+                    df = df[pred_time:pred_time]
+                    df = df.iloc[:,-1] #last column is actual variable, others are mjeh
+                    df.name = var
+                    print(f'-----{time.time()-start_timing} seconds----')
+                except:
+                    print(f"Not able to retriece the data for {var} ar {day}/{month}/{year}")
+                    df_dict = {var: [np.nan]*23}
+                    index= pd.date_range(f"{year}-{month}-{day} 01:00", f"{year}-{month}-{day} 23:00", freq='h')
+                    df = pd.DataFrame(df_dict, index=index)
 
+                day_df = day_df.join(df, how="right")
                 # Make month and day integers again
                 day = int(day)
                 month = int(month)
             
             #Merge day to total dataframe
             total_df = pd.concat([total_df, day_df])
-            total_df.to_pickle("CEDA_data_CR.pickle")
+            total_df.to_pickle("CEDA_data_CR2.pickle")
+            print(f"Currently at {day}/{month}/{year}")
     total_df.to_pickle(f"CEDA_backup_{year}")
         
 f.close()
