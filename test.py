45d29088f59f5e76f0ee6ecab13d099549f70204
r0778797 <r0778797@esat.kuleuven.be>
2024-05-16 20:18:28 +0200

    HP target complete

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from optuna.trial import TrialState
 
 study = optuna.load_study(study_name="era5 | Physics: True | TL: True", storage="sqlite:///HP_target.db")
 
-print(len(study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])))
+print(len(study.get_trials(deepcopy=False, states=[TrialState.PRUNED])))
